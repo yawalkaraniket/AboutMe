6b00789e7336acde18ba85d77e96af548f19f92a
yawalkaraniket <yawalkaraniket93@gmail.com>
2018-05-13 15:18:13 +0530

    Added the fragment class

---
+++ app/src/main/java/com/aboutme/avenjr/aboutme/fragment/BlogFrgment.java
@@ -0,0 +1,24 @@
+package com.aboutme.avenjr.aboutme.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.aboutme.avenjr.aboutme.R;
+
+public class BlogFrgment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blog_frgment, container, false);
+    }
+    public static BlogFrgment newInstance() {
+
+        return (new BlogFrgment());
+
+    }
+}
---
+++ app/src/main/java/com/aboutme/avenjr/aboutme/fragment/FeedbackFragment.java
@@ -0,0 +1,20 @@
+package com.aboutme.avenjr.aboutme.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.aboutme.avenjr.aboutme.R;
+
+public class FeedbackFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_work, container, false);
+    }
+
+}
